9e324e2d181a533d2004f2d0ffd6a61ac7b34c3a
ValdecirDesenv <valdecir.neper@gmail.com>
2025-02-01 17:03:20 -0500

    fix regular update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('events').EventEmitter.defaultMaxListeners = 10;
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
@@ -21,11 +22,15 @@ if (!fs.existsSync(defaultDevicesPath)) {
 } else {
   console.log('Default devices: ', defaultDevicesPath);
 }
-const defaultDevicesData = JSON.parse(fs.readFileSync(defaultDevicesPath, 'utf8'));
+
+let defaultDevicesData = JSON.parse(fs.readFileSync(defaultDevicesPath, 'utf8'));
+const testDevicesData = JSON.parse(fs.readFileSync(defaultDevicesPath, 'utf8'));
 
 app.use(bodyParser.json());
 
 const discovery = new SonosSystem({});
+discovery.setMaxListeners(20);
+discovery.removeAllListeners('topology-change');
 
 app.use('/', sonosRoutes(discovery));
 
@@ -37,69 +42,73 @@ wss.on('connection', (ws) => {
     console.log('Default devices data updated and saved.');
   };
 
-  const newZones = discovery.zones.filter((zone) => !defaultDevicesData[zone.uuid]);
-  if (newZones.length > 0) {
-    newZones.forEach((zone) => {
+  const historical = discovery.zones.filter((zone) => !defaultDevicesData[zone.uuid]);
+  if (historical.length > 0) {
+    historical.forEach((zone) => {
       defaultDevicesData[zone.uuid] = { ...zone, keepPlaying: false };
     });
     console.log(
       'New zones added to default devices:',
-      newZones.map((zone) => zone.uuid)
+      historical.map((zone) => zone.uuid)
     );
     saveDefaultDevicesData();
   }
 
-  // Identify extra zones that are in defaultDevicesData but not in discovery.zones
-  const extraZones = Object.keys(defaultDevicesData).filter((uuid) => !discovery.zones.some((zone) => zone.uuid === uuid));
-
-  if (extraZones.length > 0) {
-    console.log('Default devices have more zones than discovery zones. Extra zones:', extraZones);
+  if (historical.length > 0) {
+    console.log('Default devices have more zones than discovery zones. historical zones:', historical);
   }
 
   // Determine the data to send based on whether discovery zones are available
-  const dataToSend = discovery.zones.length > 0 ? { zones: discovery.zones, offlineData: false } : { zones: Object.values(defaultDevicesData), offlineData: true };
+  const dataToSend = discovery.zones.length > 0 ? { zones: discovery.zones, offLineData: false } : { zones: Object.values(defaultDevicesData), offLineData: true };
 
-  // Map the zones to the response format
-  const responseData = dataToSend.zones.map((zone) => ({
-    keepPlaying: zone.keepPlaying,
+  const historicalDevices = dataToSend.zones.map((zone) => ({
+    keepPlaying: zone.keepPlaying || false,
+    name: zone.coordinator.roomName,
     uuid: zone.uuid,
-    name: zone.name,
-    id: zone.id,
+    state: zone.coordinator.state.playbackState,
     coordinator: zone.coordinator,
-    offLineZone: false,
     members: zone.members.map((member) => ({
-      uuid: member.uuid,
-      name: member.name,
       roomName: member.roomName,
       state: member.state,
     })),
   }));
 
   // Add extra zones to the response data with an offline flag
-  extraZones.forEach((uuid) => {
-    const extraZone = defaultDevicesData[uuid];
-    responseData.push({
-      keepPlaying: extraZone.keepPlaying,
-      uuid: extraZone.uuid,
-      name: extraZone.name,
-      id: extraZone.id,
-      coordinator: extraZone.coordinator,
-      members: extraZone.members.map((member) => ({
-        uuid: member.uuid,
-        name: member.name,
-        roomName: member.roomName,
-        state: member.state,
-      })),
-      offLineZone: true,
-    });
+  let zonesOff = [];
+  historical.forEach((device) => {
+    const extraZone = defaultDevicesData[device.uuid];
+    if (extraZone) {
+      zonesOff.push({
+        keepPlaying: extraZone.keepPlaying || false,
+        uuid: extraZone.uuid,
+        name: extraZone.coordinator.name,
+        coordinator: extraZone.coordinator,
+        members: extraZone.members.map((member) => ({
+          roomName: member.roomName,
+          state: member.state,
+        })),
+        offLineZone: true,
+      });
+    } else {
+      console.warn(`Extra zone with ID ${device.id} not found in default devices.`);
+    }
   });
+
+  // TODO: Remove the commented code below
+  // if (!dataToSend.offLineData) {
+  historicalDevices.forEach((device) => {
+    if (defaultDevicesData[device.uuid]) {
+      const test = defaultDevicesData[device.uuid].keepPlaying;
+      device.keepPlaying = defaultDevicesData[device.uuid].keepPlaying;
+    }
+  });
+  // }
 
   // Include a flag if extra zones exist
   const initialData = {
-    offlineData: dataToSend.offlineData,
+    offLineData: dataToSend.offLineData,
     type: 'initial',
-    data: responseData,
-    extraZones: extraZones.length > 0 ? extraZones : null, // Include extra zones if any
+    data: historicalDevices,
   };
 
   // Send the initial data to the client
@@ -110,31 +119,30 @@ wss.on('connection', (ws) => {
       const parsedMessage = JSON.parse(message);
       if (parsedMessage.type === 'toggle-update') {
         const { groupId, state } = parsedMessage;
-        console.log(`Toggle state updated: Group ID = ${groupId}, State = ${state}`);
+        if (defaultDevicesData[groupId]) {
+          console.log(`Toggle state updated: Group ID = ${groupId}, State = ${state}`);
 
-        // Find the entry in defaultDevicesData where id matches groupId
-        const deviceKey = Object.keys(defaultDevicesData).find((key) => defaultDevicesData[key].id === groupId);
-
-        if (deviceKey) {
-          defaultDevicesData[deviceKey].keepPlaying = state;
-          console.log(`Updated keepPlaying for device: ${deviceKey} (ID: ${groupId})`);
+          defaultDevicesData[groupId].keepPlaying = state;
           saveDefaultDevicesData();
         } else {
           console.warn(`Group ID ${groupId} not found in default devices.`);
         }
+        console.log(`${defaultDevicesData[groupId].coordinator.roomName}, Device ID: ${defaultDevicesData[groupId].id}, keepPlaying: ${defaultDevicesData[groupId].keepPlaying}`);
       }
     } catch (error) {
       console.error('Error handling WebSocket message:', error);
     }
   });
 
-  // Handle topology changes and send updates to the client
+  ws.on('close', () => {
+    saveDefaultDevicesData();
+    console.log('WebSocket client disconnected');
+  });
+
   discovery.on('topology-change', (zones) => {
     console.log('Topology change detected. Sending updated zones.');
     ws.send(JSON.stringify({ type: 'topology-change', data: zones }));
   });
-
-  ws.on('close', () => console.log('WebSocket client disconnected'));
 });
 
 // Start server
